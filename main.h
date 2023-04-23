4ec2c6a0504d8af3efbaa49aad05a223a9b1d332
nidal1 <touhami9@gmail.com>
2023-04-23 00:45:35 +0000

    Fix main.h

--- main.h
+++ main.h
@@ -4,41 +4,6 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-/**
- * struct flags - struct containing flags to "turn on"
- * when a flag specifier is passed to _printf()
- * @plus: flag for the '+' character
- * @space: flag for the ' ' character
- * @hash: flag for the '#' character
- */
-typedef struct flags
-{
-	int plus;
-	int space;
-	int hash;
-} flags_t;
-
-/**
- * struct printHandler - struct to choose the right function depending
- * on the format specifier passed to _printf()
- * @c: format specifier
- * @f: pointer to the correct printing function
- */
-typedef struct printHandler
-{
-	char c;
-	int (*f)(va_list ap, flags_t *f);
-} ph;
-
-int get_flag(char s, flags_t *f);
-int (*get_print(char s))(va_list, flags_t *);
-
-int print_string(va_list l, flags_t *f);
-int print_char(va_list l, flags_t *f);
-
-int print_percent(va_list l, flags_t *f);
-
 int _putchar(char c);
-int _puts(char *str);
 int _printf(const char *format, ...);
 #endif
