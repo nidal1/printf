09b389f52e76ae753d86186cb6255271d28c3582
rihab-29 <rihab.jronedi29@gmail.com>
2023-04-22 21:09:21 -0700

    0

--- main.h
+++ main.h
@@ -3,11 +3,9 @@
 
 #include <stdlib.h>
 #include <stdarg.h>
-#include <stdio.h>
-#include <unistd.h>
 
 #define UNUSED(x) (void)(x)
-#define BUFF_SIZE 1024
+#define BUFFER_SIZE 1024
 
 #define F_MINUS 1
 #define F_PLUS 2
@@ -30,6 +28,7 @@ struct fmt
 	int (*fn)(va_list, char[], int, int, int, int);
 };
 
+
 /**
  * typedef struct fmt fmt_t - Struct op
  *
@@ -38,28 +37,30 @@ struct fmt
  */
 typedef struct fmt fmt_t;
 
-
-/*prototypes*/
 int print_char(va_list arg_list);
 int print_string(va_list arg_list);
 int _putchar(char c);
 int _printf(const char *format, ...);
+int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
+	int flags, int width, int precision, int size);
 int print_binary(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_unsigned(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
-long int convert_size_unsigned(unsigned long int num, int size);
-int get_size(const char *format, int *i);
-int write_unsigned(int is_negative, int ind,
-        char buffer[], int flags, int width, int precision, int size);
-int get_flags(const char *format, int *i);
 int print_octal(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_hexa(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_upper_hexa(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
-int print_HExa(va_list types, char map_to[],char buffer[],int flags,
-		char flag_ch, int width, int precision, int size);
+int print_HExa(va_list types, char map_to[],
+char buffer[], int flags, char flag_ch, int width, int precision, int size);
+int get_flags(const char *format, int *i);
+int get_size(const char *format, int *i);
+int write_unsigned(int is_negative, int ind,
+		char buffer[], int flags, int width, int precision, int size);
+long int convert_size_number(long int num, int size);
 
-#endif /* MAIN_H */
+
+#endif
+
