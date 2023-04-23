ccdf1317614e1077bdea03022a700612163a0620
rihab-29 <rihab.jronedi29@gmail.com>
2023-04-22 19:35:39 -0700

    0

--- main.h
+++ main.h
@@ -17,19 +17,6 @@
 #define S_LONG 2
 #define S_SHORT 1
 
-/**
- * struct flags - struct containing flags to "turn on"
- * when a flag specifier is passed to _printf()
- * @plus: flag for the '+' character
- * @space: flag for the ' ' character
- * @hash: flag for the '#' character
- */
-typedef struct flags
-{
-	int plus;
-	int space;
-	int hash;
-} flags_t;
 
 /**
  * struct printHandler - struct to choose the right function depending
@@ -43,7 +30,7 @@ typedef struct printHandler
 	int (*f)(va_list ap, flags_t *f);
 } ph;
 
-int get_flag(char s, flags_t *f);
+int get_flags(const char *format, int *i)
 int (*get_print(char s))(va_list, flags_t *);
 
 int print_string(va_list l, flags_t *f);
