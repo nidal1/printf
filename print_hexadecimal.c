5a6bb907200625ca9f17e9eaa21f5bc80a538fb6
rihab-29 <rihab.jronedi29@gmail.com>
2023-04-24 10:11:51 -0700

    fix hexa

--- print_hexadecimal.c
+++ print_hexadecimal.c
@@ -11,29 +11,25 @@
  */
 int print_hexadecimal(unsigned int num, char format)
 {
-    char *hexa_chars;
-    char buff[9];
-    int i = 0, count = 0;
-
-    hexa_chars = (format == 'X') ? "0123456789ABCDEF" : "0123456789abcdef";
-
-    
-    do {
-        buff[i++] = hexa_chars[num % 16];
-        num /= 16;
-    } while (num && i < 8);
-    buff[i] = hexa_chars[num % 16];
-
-    
-    while (i < 8) {
-        buff[++i] = '0';
-    }
-
-    
-    do {
-        _putchar(buff[i]);
-        count++;
-    } while (i--);
-
-    return (count);
+	char *hexa_chars;
+	char buff[9];
+	int i = 0, count = 0;
+	
+	hexa_chars = (format == 'X') ? "0123456789ABCDEF" : "0123456789abcdef";
+	
+	do {
+		buff[i++] = hexa_chars[num % 16];
+		num /= 16;
+	} while (num);
+	
+	while (i < 8) 
+	{
+		buff[++i] = '0';
+	}
+	
+	do {
+		_putchar(buff[i]);
+		count++;
+	} while (i--);
+	return (count);
 }
