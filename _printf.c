a0b74a2aa136252a4daf5800c7d5e73832c3f975
rihab-29 <rihab.jronedi29@gmail.com>
2023-04-24 21:55:47 -0700

    latest update

--- _printf.c
+++ _printf.c
@@ -1,45 +1,61 @@
 #include "main.h"
+
 /**
- * _printf - is a function that selects the correct function to print.
- * @format: identifier to look for.
- * Return: the length of the string.
+ * _printf - Printf function
+ * @format: format string
+ * Return: number of characters printed
  */
-int _printf(const char * const format, ...)
+int _printf(const char *format, ...)
 {
-	convert_match m[] = {
-		{"%s", printf_string}, {"%c", printf_char},
-		{"%%", printf_37},
-		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
-		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
-		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
-		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
-	};
+	int i, pri = 0, char_printed = 0;
+	int flags, width, prec, size, buff_ind = 0;
+	va_list arg_list;
+	char buffer[BUFFER_SIZE];
 
-	va_list args;
-	int i = 0, j, len = 0;
-
-	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	if (format == NULL)
 		return (-1);
 
-Here:
-	while (format[i] != '\0')
+	va_start(arg_list, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
-		j = 13;
-		while (j >= 0)
+		if (format[i] != '%')
 		{
-			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
-			{
-				len += m[j].f(args);
-				i = i + 2;
-				goto Here;
-			}
-			j--;
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFFER_SIZE)
+				print_buff(buffer, &buff_ind);
+			char_printed++;
 		}
-		_putchar(format[i]);
-		len++;
-		i++;
+		else
+		{
+			print_buff(buffer, &buff_ind);
+			flags = handle_flags(format, &i);
+			width = handle_width(format, &i, arg_list);
+			prec = handle_precision(format, &i, arg_list);
+			size = handle_size(format, &i);
+			++i;
+			pri = to_print(format, &i, arg_list, buffer,
+				flags, width, prec, size);
+			if (pri == -1)
+				return (-1);
+			char_printed += pri;
+		}
 	}
-	va_end(args);
-	return (len);
+	print_buff(buffer, &buff_ind);
+	va_end(arg_list);
+
+	return (char_printed);
+}
+
+/**
+ * print_buff - Prints the contents of the buffer
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char (lenght)
+ */
+void print_buff(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
--- main.h
+++ main.h
@@ -1,49 +1,100 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-#include <stdio.h>
-#include <stdlib.h>
 #include <stdarg.h>
-#include <limits.h>
+#include <stdio.h>
 #include <unistd.h>
 
+#define UNUSED(x) (void)(x)
+#define BUFFER_SIZE 1024
 
+#define FLAG_MINUS 1
+#define FLAG_PLUS 2
+#define FLAG_ZERO 4
+#define FLAG_HASH 8
+#define FLAG_SPACE 16
+
+#define SIZE_LONG 2
+#define SIZE_SHORT 1
 
 /**
- * struct format - match the conversion specifiers for printf
- * @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
- * @f: type pointer to function for the conversion specifier
+ * struct fmt - Struct op
  *
+ * @fmt: The format
+ * @fun: The function associated
  */
-
-typedef struct format
+struct fmt
 {
-	char *id;
-	int (*f)();
-} convert_match;
+	char fmt;
+	int (*fun)(va_list, char[], int, int, int, int);
+};
 
-int printf_pointer(va_list val);
-int printf_hex_aux(unsigned long int num);
-int printf_HEX_aux(unsigned int num);
-int printf_exclusive_string(va_list val);
-int printf_HEX(va_list val);
-int printf_hex(va_list val);
-int printf_oct(va_list val);
-int printf_unsigned(va_list args);
-int printf_bin(va_list val);
-int printf_srev(va_list args);
-int printf_rot13(va_list args);
-int printf_int(va_list args);
-int printf_dec(va_list args);
-int _strlen(char *s);
-int *_strcpy(char *dest, char *src);
-int _strlenc(const char *s);
-int rev_string(char *s);
-int _strlenc(const char *s);
-int printf_37(void);
-int printf_char(va_list val);
-int printf_string(va_list val);
-int _putchar(char c);
+/**
+ * typedef struct fmt fun_t - Struct op
+ *
+ * @fmt: The format.
+ * @fun_t: The function associated
+ */
+typedef struct fmt fun_t;
+
+/* printf prototype */
 int _printf(const char *format, ...);
+void print_buff(char buffer[], int *buff_ind);
 
-#endif
+/* to_print.c prototypes */
+int to_print(const char *format, int *i, va_list arg_list, char buffer[],
+		int flags, int width, int prec, int size);
+
+/* printf_functions1.c prototypes */
+int print_char(va_list args, char buffer[],
+	int flags, int width, int prec, int size);
+int print_string(va_list args, char buffer[],
+	int flags, int width, int prec, int size);
+int print_percent(va_list args, char buffer[],
+	int flags, int width, int prec, int size);
+int print_int(va_list args, char buffer[],
+	int flags, int width, int prec, int size);
+int print_binary(va_list args, char buffer[],
+	int flags, int width, int prec, int size);
+/* printf_functions2.c prototypes */
+int print_unsigned(va_list args, char buffer[],
+	int flags, int width, int prec, int size);
+int print_octal(va_list args, char buffer[],
+	int flags, int width, int prec, int size);
+int print_hexa(va_list args, char buffer[],
+	int flags, int width, int prec, int size);
+int print_HEXA(va_list args, char buffer[],
+	int flags, int width, int prec, int size);
+int print_HExa(va_list args, char a[], char buffer[],
+		int flags, char flag_c, int width, int prec, int size);
+/* printf_functions3.c prototypes */
+int print_nonprintable(va_list args, char buffer[],
+	int flags, int width, int prec, int size);
+int print_adress(va_list args, char buffer[],
+	int flags, int width, int prec, int size);
+int print_rot13(va_list args, char buffer[],
+		int flags, int width, int prec, int size);
+int print_rev(va_list args, char buffer[],
+		int flags, int width, int prec, int size);
+/* handlers prototypes */
+int handle_flags(const char *formatt, int *i);
+int handle_width(const char *format, int *i, va_list arg_list);
+int handle_precision(const char *format, int *i, va_list arg_list);
+int handle_size(const char *format, int *i);
+int write_char_handler(char c, char buffer[],
+	int flags, int width, int prec, int size);
+int write_num_handler(int _negative, int ind, char buffer[],
+	int flags, int width, int prec, int size);
+int write_number(int ind, char buffer[], int flags, int width, int prec,
+	int len, char p, char ex_c);
+int write_pointer(char buffer[], int ind, int len,
+	int width, int flags, char p, char ex_c, int p_start);
+int write_unsignd(int _negative, int ind, char buffer[],
+		int flags, int width, int prec, int size);
+/* outils.c prototypes */
+int _printable(char);
+int add_acsii(char, char[], int);
+int _digit(char);
+long int size_number(long int num, int size);
+long int size_unsignd(unsigned long int num, int size);
+
+#endif /* MAIN_H */
